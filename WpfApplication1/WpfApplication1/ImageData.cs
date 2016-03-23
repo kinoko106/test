7b5078d08d6c4cc36e556b9fdd3852123ca905a1
kinoko106 <03daichi28@gmail.com>
2016-03-23 18:05:00 +0900

    UI進んだ

--- WpfApplication1/WpfApplication1/ImageData.cs
+++ WpfApplication1/WpfApplication1/ImageData.cs
@@ -25,9 +25,34 @@ namespace WpfApplication1
 
 			Width = MatImage.Width;
 			Height = MatImage.Height;
+
+			resizeImage = new Mat();
+			if (MatImage.Width > 600 || MatImage.Height > 600)
+			{
+				double range;
+				Size s;
+				if(MatImage.Height > MatImage.Width)
+				{
+					range = 600.0 / MatImage.Height;
+					s = new Size(MatImage.Width * range, 600);
+				}
+				else
+				{
+					range = 600.0 / MatImage.Width;
+					s = new Size(600,MatImage.Height * range);
+				}
+				
+				Cv2.Resize(MatImage,resizeImage,s);
+			}
+			else
+			{
+				resizeImage = MatImage.Clone();
+			}
+			
 		}
 
 		public Mat MatImage { get; set; }
+		public Mat resizeImage { get; set; }
 		public string FileName { get; set; }
 		public int Height { get; set; }
 		public int Width { get; set; }
