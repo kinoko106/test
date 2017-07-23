8b3262354f9ca27452639e02c0ff301f908c6f77
daichi <daichi@DESKTOP-6QPU3F7>
2017-07-23 17:59:55 +0900

    Strechできないマン

---
+++ TCPServer/TCPServer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TCPServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IPEndPoint ipAdd = new IPEndPoint(IPAddress.Parse("192.168.0.3"), 8888);
+			TcpListener listener = new TcpListener(ipAdd);
+			try
+			{
+				listener.Start(0);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			Console.WriteLine("Port:8888のListenを開始しました。");
+
+			TcpClient client = listener.AcceptTcpClient();
+			Console.WriteLine("クライアントが接続しました。");
+
+			if (client.Connected)
+			{
+				listener.Stop();
+				NetworkStream netStream = client.GetStream();
+				StreamReader sReader = new StreamReader(netStream, Encoding.UTF8);
+
+				string str = String.Empty;
+
+				do
+				{
+					str = sReader.ReadLine();
+					if (null == str)
+					{
+						break;
+					}
+					Console.WriteLine(str);
+				} while (!str.Equals("quit"));
+				sReader.Close();
+				client.Close();
+			}
+			Console.WriteLine("終了するには、Enterキーを押してください");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ TCPTest/TCPTest/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace TCPTest
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public string filePath;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void grid_PreviewDragOver(object sender, System.Windows.DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(System.Windows.DataFormats.FileDrop, true))
+			{
+				e.Effects = System.Windows.DragDropEffects.Copy;
+			}
+			else
+			{
+				e.Effects = System.Windows.DragDropEffects.None;
+			}
+			e.Handled = true;
+		}
+		private void grid_Drop(object sender, System.Windows.DragEventArgs e)
+		{
+			var dropFiles = e.Data.GetData(System.Windows.DataFormats.FileDrop) as string[];
+			if (dropFiles == null) return;
+			filePath = dropFiles[0];
+			textBox2.Text = dropFiles[0];
+
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			using (BinaryReader br = new BinaryReader(File.OpenRead(filePath)))
+			{
+				List<byte> fileData = new List<byte>();
+				try
+				{
+					for (;;)
+					{
+						fileData.Add(br.ReadByte());
+					}
+				}
+				catch (EndOfStreamException)
+				{
+					
+				}
+			}
+
+			textBlock3.Text = sw.ElapsedMilliseconds.ToString();
+			//var directry = System.IO.Directory.GetParent(filePath);
+			//string fileName = System.IO.Path.GetFileName(filePath);
+			//string newFile = @directry.FullName + @"\newfile.txt";
+		}
+	}
+}
